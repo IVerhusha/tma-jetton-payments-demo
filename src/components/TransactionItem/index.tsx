0f87239d74dbab985bfe82bc70952e36fbbb3a1c
igor <i.verhusha@pixelplex.io>
2024-05-30 20:02:25 +0400

    hotfix: transaction card

--- src/components/TransactionItem/index.tsx
+++ src/components/TransactionItem/index.tsx
@@ -17,17 +17,19 @@ const TransactionItem = ({ tx }: Props) => {
   return (
     <div className={styles.card}>
       <div className={styles.content}>
-        <button
-          className={styles.link}
-          onClick={() => utils.openLink(`https://${network === CHAIN.TESTNET ? 'testnet.' : ''}tonviewer.com/transaction/${tx.hash}`)}
-        >
-          See transaction
-        </button>
+        <div className={styles.header}>
+          <button
+            className={styles.link}
+            onClick={() => utils.openLink(`https://${network === CHAIN.TESTNET ? 'testnet.' : ''}tonviewer.com/transaction/${tx.hash}`)}
+          >
+            See transaction
+          </button>
+          <div className={clsx(styles.label, { [styles.failed]: tx.status === 'failed' })}>{tx.status}</div>
+        </div>
         <p>Price: ${calculateUsdFromUsdt(tx.usdtAmount).toString()}</p>
-        <p>Status: <span className={clsx(styles.label, {[styles.failed]: tx.status === 'failed'})}>{tx.status}</span></p>
       </div>
     </div>
-);
+  );
 };
 
 export default TransactionItem;
