6d617746e9fafac3cec0ffa38e3e43da336c4017
AlexG <39581753+reveloper@users.noreply.github.com>
2024-06-17 18:30:58 +0900

    Update common-constants.ts
    update_usdtt_testnet_master_address

--- src/constants/common-constants.ts
+++ src/constants/common-constants.ts
@@ -1,4 +1,4 @@
 import { Address } from '@ton/core';
 
-export const USDT_MASTER_ADDRESS = Address.parse('kQC2sWxkwfqwsb7O3z-lRInJQO1f11qDDd8jD5wrcg27ss5h');
+export const USDT_MASTER_ADDRESS = Address.parse('kQD0GKBM8ZbryVk2aESmzfU6b9b_8era_IkvBSELujFZPsyy');
 export const INVOICE_WALLET_ADDRESS = Address.parse('0QD-SuoCHsCL2pIZfE8IAKsjc0aDpDUQAoo-ALHl2mje04A-');
